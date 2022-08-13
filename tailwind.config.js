81403a837de5649b60a7b332f6155472e9a8739c
Ayaan <kaifullah7921@gmail.com>
2022-08-13 22:51:25 +0530

    tailwind configs

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,24 +1,8 @@
-// tailwind.config.js
+/** @type {import('tailwindcss').Config} */ 
 module.exports = {
+  content: ["./src/**/*.{html,js}"],
   theme: {
-    colors: {
-      gray: colors.coolGray,
-      blue: colors.lightBlue,
-      red: colors.rose,
-      pink: colors.fuchsia,
-    },
-    fontFamily: {
-      sans: ['Graphik', 'sans-serif'],
-      serif: ['Merriweather', 'serif'],
-    },
-    extend: {
-      spacing: {
-        '128': '32rem',
-        '144': '36rem',
-      },
-      borderRadius: {
-        '4xl': '2rem',
-      }
-    }
-  }
+    extend: {},
+  },
+  plugins: [],
 }
