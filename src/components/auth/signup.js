f2e9722ade9f0f472e238dfbe6e82ebd5b4249f4
Ayaan <kaifullah7921@gmail.com>
2022-08-13 21:26:22 +0530

    logic set up for signup with appwrite

--- src/components/auth/signup.js
+++ src/components/auth/signup.js
@@ -1,7 +1,33 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import { account } from "../../appwrite/appwrite.config";
+import { useNavigate } from "react-router-dom";
+import { v4 as uuidv4 } from "uuid";
 
 export default function signup() {
-  return (
-    <div>signup</div>
-  )
+  const navigate = useNavigate();
+  const [user, setUser] = useState({
+    name: "",
+    email: "",
+    password: "",
+  });
+  const userSignup = async (e) => {
+    e.preventDefault();
+
+    const promise = account.create(
+      uuidv4(),
+      user.email,
+      user.password,
+      user.name
+    );
+    promise.then(
+      (response) => {
+        Console.log(response);
+        navigate("/login");
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
+  };
+  return <div>signup</div>;
 }
