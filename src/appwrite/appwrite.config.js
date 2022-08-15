b196d1578665f515ed5e4e7d36760ef39f343628
Ayaan Kaifullah <60045274+Ayaan2907@users.noreply.github.com>
2022-08-16 00:03:18 +0530

    updating config with latest config.
    a bad option, instead of  .env

--- src/appwrite/appwrite.config.js
+++ src/appwrite/appwrite.config.js
@@ -2,7 +2,7 @@ export const config = {
   END_POINT: "https://43.204.234.214/v1",
   PROJECT_ID: "62fa8eb0bd65a0e831de",
   COLLECTION_ID: "62fa8f82280d658f8a44",
-  DATABASE_ID : "62f73a5a1202ee1e4ab1"//pending
+  DATABASE_ID : "62fa8eb0bd65a0e831de"//pending
 };
 
 // endpoint : process.env.REACT_APP_ENDPOINT,
