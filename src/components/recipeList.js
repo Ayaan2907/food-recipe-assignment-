d0c835e0d9fb1c39ef67a666b009f4a3b4271ad8
Ayaan <kaifullah7921@gmail.com>
2022-08-15 11:54:03 +0530

    fetching docs from DB done, UI incomplete

--- src/components/recipeList.js
+++ src/components/recipeList.js
@@ -1,10 +1,42 @@
-import React from "react";
-import Recipe from "./recipe";
-export default function recipeList() {
+import React, { useEffect, useState } from "react";
+import RecipeCard from "./recipeCard";
+import { databases } from "../appwrite/api";
+import { config } from "../appwrite/appwrite.config";
+export default function RecipeList() {
+  const [recipies, setRecipies] = useState();
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    setLoading(true);
+    const getRecipiesPromise = databases.listDocuments(config.COLLECTION_ID);
+    getRecipiesPromise.then(
+      (response) => {
+        setRecipies(response.documents);
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
+    setLoading(false);
+  }, []);
+
   return (
+    // TODO: UI
     <>
-      <div>recipe list</div>
-      <Recipe />
+      {loading ? (
+        <div>loading...</div>
+      ) : (
+        <div>
+          <div>Recipe list</div>
+          {recipies &&
+            recipies.map((recipe) => (
+              <div key={recipe.$id}>
+                <p> {recipe.name} </p>
+              </div>
+            ))}
+          {/* <RecipeCard key={recipe.$id} recipe={recipe} /> */}
+        </div>
+      )}
     </>
   );
 }
